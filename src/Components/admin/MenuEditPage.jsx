6adf3583cf2822604add799897d4466bed10acc7
first <garvitjainraju@gmail.com>
2024-09-03 02:19:20 +0530

    new

---
+++ src/Components/admin/MenuEditPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MenuEditPage = () => {
+  return (
+    <div>MenuEditPage</div>
+  )
+}
+
+export default MenuEditPage
--- src/Pages/AdminMenuPage.jsx
+++ src/Pages/AdminMenuPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import Dashboard from "../Components/admin/Dashboard";
+import AdminSidebar from "../Components/admin/AdminSidebar";
+import AdminMenu from "../Components/admin/AdminMenu";
+import MenuEditPage from "../Components/admin/MenuEditPage";
 
 const AdminMenuPage = () => {
+  const [edit, setedit] = useState("");
   return (
-    <div>AdminMenuPage</div>
-  )
-}
+    <div className="w-full h-screen bg-[#EEEEEE] flex">
+      <AdminSidebar />
+      <div className="w-full h-full overflow-y-auto  relative">
+        <div className="flex w-full items-center justify-between font-bold text-[#6E39CB] p-7">
+          <h1>Menu</h1>
+          <h1>Wednesday, 12 Jul 2024 </h1>
+        </div>
+        {edit ? <MenuEditPage /> : <AdminMenu />}
+      </div>
+    </div>
+  );
+};
 
-export default AdminMenuPage
+export default AdminMenuPage;
