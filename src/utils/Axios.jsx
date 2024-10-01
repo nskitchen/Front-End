08a021a21271112872d7318a3293924c0259da58
Anurag Sarkar <sarkaranurag104@gmail.com>
2024-10-01 14:43:44 +0530

    env

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Axios instance for User API
-const hostName = process.env.NODE_ENV === "development" ? "http://localhost:8001" : process.env.URL;
+const hostName = process.env.NODE_ENV === "development" ? "http://localhost:8001" : process.env.REACT_APP_API_URL;
 export const userAPI = axios.create({
   baseURL: hostName + "/api/v1/users",
   timeout: 5000,
